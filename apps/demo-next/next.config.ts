01bb4c9de49df0ada580fbfb5c840eb5563c6df8
designerSejinOH <ohsj1999@naver.com>
2026-02-22 18:59:17 +0900

    fix demo-next external path

--- apps/demo-next/next.config.ts
+++ apps/demo-next/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpilePackages: ["@happyphysicsclub/r3f-director"],
+  experimental: {
+    externalDir: true,
+  },
 };
 
 export default nextConfig;
